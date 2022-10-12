2c474c77c49a6f1cbd6ba75f360bf93c405ec2ef
Vina Tan <88601826+vinatsx@users.noreply.github.com>
2022-10-12 20:02:18 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# Create your own db.py with mongodb connection details, DO NOT commit that file
+# PM me on telegram your connection string, username and password
+
+from db import db
+
+# Create
+
+# Read
+
+# Update
+
+# Delete
